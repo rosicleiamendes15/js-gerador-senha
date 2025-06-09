f822422e9a8ba6e8b21a4f18ff3e30b6f27090d4
rosicleiamendes15 <149099389+rosicleiamendes15@users.noreply.github.com>
2025-06-09 11:28:27 -0300

    .

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ const numeroSenha = document.querySelector('.parametro-senha_texto');
 numeroSenha.textContent = 5;
 let tamanhoSenha = 12;
 const botoes = document.querySelectorAll('.parametro-senha_botao');
-botoes[0]onclick = diminuirTamanho;
-botoes[0]onclick = aumentarTamanho;
+botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentarTamanho;
 
 
 console.log(botoes);
 
 function diminuirTamanho(){
+    if (tamanhoSenha > 1)
     tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
+}
+function aumentarTamanho(){
+    if(tamanhoSenha < 20)
+  tamanhoSenha = tamanhoSenha + 1;
     numeroSenha.textContent = tamanhoSenha
 }
